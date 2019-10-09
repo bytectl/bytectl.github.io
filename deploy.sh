8bd35cdfc5ad01838ecf0d0728eef27ec2e2457c
wuxingzhong <330332812@qq.com>
2019-10-09 14:45:26 +0800

    modify

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ git add .
 git commit -m add
 git push
 hugo
-cp -r public/* ~/work/src/github.com/sealyun/sealyun.github.io 
-cd ~/work/src/github.com/sealyun/sealyun.github.io
+
 git add .
 git commit -m add
 git push
